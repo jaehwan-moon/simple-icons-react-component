5bc436eb4da772a78a3a4a6d5018d7480306f9e2
Jaehwan Moon <mkch115@gmail.com>
2019-02-14 10:22:03 -0500

    test(SimpleIcon): Setup the test for the component

---
+++ test/SimpleIcons.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import simpleIcons from 'simple-icons';
+
+import SimpleIcon from '../lib/SimpleIcon';
+
+describe('SimpleIcon', () => {
+  it('should use default icon color when there is no custom color prop', () => {
+    const wrapper = shallow(<SimpleIcon name="React" />);
+    const reactIconColor = `#${simpleIcons['React'].hex}`;
+
+    expect(wrapper.find('div').prop('style').color).toBe(reactIconColor);
+  })
+
+  it('should get a custom color as a prop', () => {
+    const wrapper = shallow(<SimpleIcon name="React" color="#a3b8f3" />);
+    expect(wrapper.find('div').prop('style').color).toBe('#a3b8f3');
+  });
+  
+  it('should return null when there is no matching name of an icon', () => {
+    const wrapper = shallow(<SimpleIcon name="Not Existing" />);
+    expect(wrapper.type()).toEqual(null);
+  });
+})
